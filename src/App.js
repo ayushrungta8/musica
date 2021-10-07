bf52127069c60f6c981beda70c11a6ff4e64baf0
Ayush Rungta <ayushrungta8@gmail.com>
2021-10-07 12:31:08 +0530

    Setup Boilerplate Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Content from "./components/content/Content";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <Sidebar />
+      <Content></Content>
+    </Container>
   );
 }
 
+const Container = styled.div`
+  display: flex;
+`;
 export default App;
---
+++ src/components/content/Content.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "../footer/Footer";
+import Header from "../header/Header";
+
+const Content = () => {
+  return (
+    <div>
+      Hello Content
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default Content;
